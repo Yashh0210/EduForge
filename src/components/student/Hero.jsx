60541383f024cf6a67121c3201e49100cda5c22f
Yashh0210 <yashyadavpw@gmail.com>
2025-11-17 23:53:27 +0530

    updated hero section of student

--- src/components/student/Hero.jsx
+++ src/components/student/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <img src={assets.sketch} alt="sketch" className="md:block hidden absolute -bottom-7 right-0" />
       </h1>
       <p className="md:block hidden text-gray-500 max-w-2xl mx-auto">
-        We bring together world-class instructors, interactive content, and a supportive community to help you achieve your personal and professional goals.
+        We bring together world-class instructors , interactive content, and a supportive community to help you achieve your personal and professional goals.
       </p>
       <p className="md:hidden text-gray-500 max-w-sm mx-auto">
         We bring together world-class instructors to help you achieve your professional goals.
